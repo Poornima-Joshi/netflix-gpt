8c1966618325d1d540fe33bf0b30e8b84c65d9c4
Poornima-Joshi <poornima.joshi776@gmail.com>
2024-03-07 18:56:38 +0530

    firebase setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {getAuth} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDXWVoUq4kYbVz2cGdnuaepRZbVt2LjL7A",
-  authDomain: "poornima-joshi-netflixgpt.firebaseapp.com",
-  projectId: "poornima-joshi-netflixgpt",
-  storageBucket: "poornima-joshi-netflixgpt.appspot.com",
-  messagingSenderId: "418850370478",
-  appId: "1:418850370478:web:46417e01d7a7a4de2d0e3f",
-  measurementId: "G-BTM3NV4CTP"
+  apiKey: "AIzaSyDjTzN4--EL00aTCfmf2MCUwexOMCIY9HE",
+  authDomain: "poornima-joshi.firebaseapp.com",
+  projectId: "poornima-joshi",
+  storageBucket: "poornima-joshi.appspot.com",
+  messagingSenderId: "439910404173",
+  appId: "1:439910404173:web:ceed6ca7555b32596f4c04",
+  measurementId: "G-CZC08ZF303"
 };
 
 // Initialize Firebase
